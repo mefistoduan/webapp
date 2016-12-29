c834becd8d268531cd135a7ee67f754159a4f435
Changpeng Duan <838560574@qq.com>
2016-12-29 08:18:27 +0800

    finish

--- src/module/finish/finish.vue
+++ src/module/finish/finish.vue
@@ -4,10 +4,10 @@
         </div>
         <h5>订单详情</h5>
         <div class="order-tip">
-            <h3>订单提交成功</h3>
-            <p>订单号：p2016122741710</p>
-            <p>应付金额：<i>0元</i></p>
-            <p>支付方式：余额支付</p>
+            <h3>{{orderState}}</h3>
+            <p>订单号：{{orderNum}}</p>
+            <p>应付金额：<i>{{orderPrice}}元</i></p>
+            <p>支付方式：{{orderPayType}}</p>
         </div>
         <div class="pay-tip" style="margin-bottom:0px">
             <p>请在24小时内完成支付，否则订单将会被自动取消。<br>
@@ -16,9 +16,9 @@
         <div class="order_tips" style="margin-bottom:0px">
             <p>请收货时核对产品和数量与订单产品是否一致！</p>
         </div>
-        <router-link to="/finish" class="jump_part">
+        <router-link to="/finish" class="jump_part bottom_link">
             <p>
-                <input autocomplete="off" value="查看订单" class="sub_btn_y sub_btn">
+            <div autocomplete="off" value="done" class="sub_btn_y sub_btn">查看订单</div>
             </p>
         </router-link>
     </div>
@@ -31,10 +31,10 @@
     export default {
         data() {
             return {
-                list: [
-                    {text: '麦肯',text:'png',link:'http://www.emeixian.com/mobile/brands.php?b_id=2' }
-                ],
-                showVariable:false
+                orderState:'订单提交成功',
+                orderNum:'p2016122741710',
+                orderPrice:525.50,
+                orderPayType:'余额支付'
             }
 
         },
@@ -51,41 +51,77 @@
 </script>
 
 
-<style lang="stylus" scoped>
+<style lang="stylus" rel="stylesheet/stylus" scoped>
     @import url('../../assets/css/bootstrap.min.css')
 body
     padding-top 5px
-    .order-tip {
-        width: 85%;
-        padding: 10px;
-        margin: 20px auto;
-        background: #fffdf7;
-        border: 1px solid #cdc2b0;
-        border-radius: 5px;
-        -webkit-border-radius: 5px;
-        -moz-border-radius: 5px;
-    }
-        h3 {
-            color: #48AE41;
-            font-size: 1em;
-            font-weight: normal;
-        }
+    h5
+        text-align center
+        font-size 2em
+        padding-top 20px
+    .order-tip
+        width 85%
+        padding 10px
+        margin 20px auto
+        background #fffdf7
+        border 1px solid #cdc2b0
+        border-radius 5px
+        -webkit-border-radius 5px
+        -moz-border-radius 5px
+
+        h3
+            color #48AE41
+            font-size 1em
+            font-weight normal
+
     i
         color #1eaa39
         font-weight bold
-    .pay-tip {
-        width: 85%;
-        margin: 5px auto;
-        padding: 10px;
-        background: #f0ead4;
-        border: 1px solid #a4a4a4;
-        border-radius: 5px;
-        -webkit-border-radius: 5px;
-        -moz-border-radius: 5px;
-    }
+    .pay-tip
+        width 85%
+        margin 5px auto
+        padding 10px
+        background #f0ead4
+        border 1px solid #a4a4a4
+        border-radius 5px
+        -webkit-border-radius 5px
+        -moz-border-radius 5px
+
     .order_tips
         p
+            width 90%
+            display block
+            margin 0 auto
+            text-align center
+            color rgb(255,0,0)
+    .sub_btn_y
+        color #2b2b2b
+        text-shadow 1px 0 0 #fff
 
+    .sub_btn
+        display block
+        background-color red
+        border none
+        width 90%
+        margin 10px auto 0
+        height 40px
+        line-height 40px
+        color #fff
+        font-size 1em
+        font-weight bold
+        cursor pointer
+        text-align center
+        border-radius 5px
+        -webkit-border-radius 5px
+        -moz-border-radius 5px
+        text-shadow 1px 0 0 #000
+        border-bottom 1px solid #bcb0a3
+.bottom_link
+    width 60%
+    overflow hidden
+    display block
+    margin 0 auto
+    float none
 
 
 </style>
